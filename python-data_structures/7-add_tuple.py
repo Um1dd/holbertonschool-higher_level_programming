1f275f537937f16c6df42d0671471e27601b583d
Um1dd <umudnagili9@gmail.com>
2026-04-11 13:08:51 +0000

    Fix pycodestyle for task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Hər tuple üçün ilk iki elementi təmin edirik (çatışmırsa 0 əlavə edirik)
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
-    # Yeni tuple yaradırıq: ilk iki elementin cəmi
-    result = (a[0] + b[0], a[1] + b[1])
-    
-    return result
+    return (a[0] + b[0], a[1] + b[1])
