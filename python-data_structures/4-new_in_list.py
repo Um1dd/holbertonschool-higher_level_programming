5debd1f05ba869f560aeb57ea3326eed06f7c602
Um1dd <umudnagili9@gmail.com>
2026-04-11 12:59:37 +0000

    Fix pycodestyle whitespace errors

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Siyahının kopyasını yaradırıq
     copy_list = my_list[:]
-    
-    # İndeks yoxlaması
     if idx < 0 or idx >= len(my_list):
         return copy_list
-    
-    # Dəyişikliyi yalnız kopya üzərində edirik
     copy_list[idx] = element
     return copy_list
