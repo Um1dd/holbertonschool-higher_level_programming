87945740eaebc14882cea5f3cc4031dffb60e860
root <umudnagili9@gmail.com>
2026-04-11 19:48:50 +0000

    Fixing documentation and style for task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,31 +1,41 @@
 #!/usr/bin/python3
-"""Square sinfi üçün modul"""
+"""
+Bu modul Square sinfini təyin edir.
+"""
 
 
 class Square:
-    """Kvadratı təmsil edən sinif"""
+    """
+    Kvadratı təmsil edən sinif.
+    """
 
     def __init__(self, size=0):
         """
         Yeni bir Square obyekti yaradır.
 
         Args:
-            size (int): Kvadratın tərəfinin ölçüsü (default 0).
+            size (int): Kvadratın tərəfinin ölçüsü.
         """
         self.size = size
 
     @property
     def size(self):
-        """Kvadratın ölçüsünü qaytarır (Getter)"""
+        """
+        Kvadratın ölçüsünü qaytarır (Getter).
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
         """
-        Kvadratın ölçüsünü təyin edir (Setter).
-        
+        Kvadratın ölçüsünü təyin edir və yoxlayır (Setter).
+
         Args:
             value (int): Yeni ölçü dəyəri.
+
+        Raises:
+            TypeError: Əgər dəyər integer deyilsə.
+            ValueError: Əgər dəyər 0-dan kiçikdirsə.
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -34,5 +44,10 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Kvadratın sahəsini hesablayır və qaytarır"""
+        """
+        Kvadratın sahəsini hesablayır.
+
+        Returns:
+            int: Sahə dəyəri.
+        """
         return self.__size ** 2
