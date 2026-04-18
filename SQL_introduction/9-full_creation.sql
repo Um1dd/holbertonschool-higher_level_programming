5e5ab29b5c821d677294e04c05703e67e597fd26
Um1dd <umudnagili9@gmail.com>
2026-04-18 18:18:42 +0000

    Fix: Add comments and format for Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,11 @@
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- Creates a table second_table in the database hbtn_0c_0
+-- and adds multiple rows for John, Alex, Bob, and George
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
