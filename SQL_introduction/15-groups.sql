bd2285d7cf7a4bbf492c2d1137f7b526a026c6f4
Um1dd <umudnagili9@gmail.com>
2026-04-24 05:52:59 +0000

    Task 15: Group records by score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Lists the number of records with the same score in the table second_table
+-- Results display the score and the number of records for this score
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
