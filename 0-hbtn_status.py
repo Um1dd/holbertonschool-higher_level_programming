736c3675353eb9840ee431d5c04935b1d481c698
Um1dd <umidnaghiyev9@gmail.com>
2026-08-29 23:22:19 +0300

    Add 0-hbtn_status.py script

--- 0-hbtn_status.py
+++ 0-hbtn_status.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-"""
-This module fetches the status from https://intranet.hbtn.io/status
-using urllib, including the required bypass header.
-"""
-
+"""Fetches https://intranet.hbtn.io/status using urllib"""
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    headers = {'cfclearance': 'true'}
-
-    req = urllib.request.Request(url, headers=headers)
-
-    with urllib.request.urlopen(req) as response:
-        body = response.read()
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
         print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decode('utf-8')}")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
