67cd833a8a3b94880c68d877667ee44503081ae7
Um1dd <umidnaghiyev9@gmail.com>
2026-08-30 22:43:56 +0300

    Add python script for response header value

---
+++ 1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader('X-Request-Id'))
