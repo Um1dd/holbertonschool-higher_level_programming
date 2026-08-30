c2fcc41095a281144dddf12c6bfc2bf3569100cd
Um1dd <umidnaghiyev9@gmail.com>
2026-08-30 22:47:42 +0300

    Fix 1-hbtn_header.py script and header lookup

--- 1-hbtn_header.py
+++ 1-hbtn_header.py
@@ -10,6 +10,6 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
-        header_val = response.getheader('X-Request-Id')
-        if header_val is not None:
+        header_val = response.headers.get('X-Request-Id')
+        if header_val:
             print(header_val)
