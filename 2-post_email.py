b36c9acca2965a8474098a9cdea48c70ee6eea80
Um1dd <umidnaghiyev9@gmail.com>
2026-08-30 22:48:37 +0300

    Add 2-post_email.py script

---
+++ 2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response.
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    
+    values = {'email': email}
+    data = urllib.parse.urlencode(values).encode('ascii')
+    
+    req = urllib.request.Request(url, data)
+    req.add_header('cfclearance', 'true')
+    
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
