ad9c37b421462cd67793dc4d9bd1928957d0d4a6
Um1dd <umidnaghiyev9@gmail.com>
2026-09-06 09:39:48 +0300

    Add 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and email, sends a POST request
+This module takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter, and displays
-the body of the response.
+the body of the response decoded in utf-8.
 """
 import sys
 import urllib.parse
 import urllib.request
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data)
-    req.add_header('cfclearance', 'true')
+
+    payload = {'email': email}
+    data = urllib.parse.urlencode(payload).encode('utf-8')
+
+    req = urllib.request.Request(
+        url,
+        data=data,
+        headers={'cfclearance': 'true'}
+    )
+
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
