345a3d6f324e1003f109599d38ced86e6f5fa637
Um1dd <umidnaghiyev9@gmail.com>
2026-09-06 09:24:46 +0300

    Add 0-hbtn_status.py script

--- 0-hbtn_status.py
+++ 0-hbtn_status.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using urllib"""
+"""
+This module fetches the status of the Holberton intranet page
+using urllib and displays details about the body response.
+"""
 import urllib.request
+
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
+    req = urllib.request.Request(url, headers={'cfclearance': 'true'})
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
         print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
