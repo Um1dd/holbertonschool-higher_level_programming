41d0ef953edc42aca36457c767f3c37924716472
Um1dd <umidnaghiyev9@gmail.com>
2026-09-06 09:38:05 +0300

    Add 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL
+This module takes in a URL, sends a request to the URL,
 and displays the value of the X-Request-Id variable
 found in the header of the response.
 """
 import sys
 import urllib.request
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        header_val = response.headers.get('X-Request-Id')
-        if header_val:
-            print(header_val)
+    req = urllib.request.Request(url, headers={'cfclearance': 'true'})
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get('X-Request-Id'))
